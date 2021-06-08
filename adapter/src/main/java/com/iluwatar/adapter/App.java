25f829be9e23b3b641546023b51bb336eadaa5e4
whalenchan <whalenchan330@gmail.com>
2021-06-08 09:41:49 +0800

    增加适配器模式笔记和注释

--- adapter/src/main/java/com/iluwatar/adapter/App.java
+++ adapter/src/main/java/com/iluwatar/adapter/App.java
@@ -41,21 +41,31 @@ package com.iluwatar.adapter;
  * new ship! we need to reuse this {@link FishingBoat}. The captain needs a rowing boat which he can
  * operate. The spec is in {@link RowingBoat}. We will use the Adapter pattern to reuse {@link
  * FishingBoat}.
+ *
+ * <p>适配器帮助两个不兼容的接口一起工作。这是适配器的真实定义 接口可能不兼容，但内部功能应该适合需要。
+ * 适配器设计模式允许其他不兼容的类通过将一个类的接口转换为客户端期望的接口来协同工作。
+ * <p>适配器模式有两种变体：类适配器实现适配器的接口，而对象适配器使用组合将适配器包含在适配器对象中。
+ * 此示例使用对象适配器方法。
+ * <p>适配器（{@link FishingBoatAdapter}）将被适配类（{@link FishingBoat}）的接口转换为客户端期望的合适的接口（{@link RowingBoat}）。
+ * <p>这个实现的故事是这样的。海盗来了！我们需要 {@link RowingBoat} 才能逃跑！我们有一艘 {@link FishingBoat} 和我们的船长。
+ * 我们来不及造一艘新船了！我们需要重用这个 {@link FishingBoat}。船长需要一艘他可以操作的划艇。
+ * 规范在 {@link RowingBoat} 中。我们将使用适配器模式来重用 {@link FishingBoat}。
  */
 public final class App {
 
-  private App() {
-  }
+    private App() {
+    }
 
-  /**
-   * Program entry point.
-   *
-   * @param args command line args
-   */
-  public static void main(final String[] args) {
-    // The captain can only operate rowing boats but with adapter he is able to
-    // use fishing boats as well
-    var captain = new Captain(new FishingBoatAdapter());
-    captain.row();
-  }
+    /**
+     * Program entry point.
+     *
+     * @param args command line args
+     */
+    public static void main(final String[] args) {
+        // The captain can only operate rowing boats but with adapter he is able to
+        // use fishing boats as well
+        //船长只能操作划艇，但通过适配器他也可以使用渔船
+        var captain = new Captain(new FishingBoatAdapter());
+        captain.row(); //The fishing boat is sailing
+    }
 }
