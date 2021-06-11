358c3e5673d5b0fe7336beb2ee02b53de5303e7f
whalenchan <whalenchan330@gmail.com>
2021-06-11 10:21:34 +0800

    *增加异步方法调用模式注释*

--- async-method-invocation/src/main/java/com/iluwatar/async/method/invocation/App.java
+++ async-method-invocation/src/main/java/com/iluwatar/async/method/invocation/App.java
@@ -23,8 +23,9 @@
 
 package com.iluwatar.async.method.invocation;
 
-import java.util.concurrent.Callable;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
 
 /**
  * In this example, we are launching space rockets and deploying lunar rovers.
@@ -49,6 +50,20 @@ import lombok.extern.slf4j.Slf4j;
  * take all possible scenarios into account but rather provides a simple version that helps to
  * understand the pattern.
  *
+
+ * <p>异步方法调用模式
+ * <p>在这个例子中，我们发射太空火箭并部署月球车。
+ * <p>该应用程序演示了异步方法调用模式。 该模式的关键部分是 <code>AsyncResult</code>，它是一个异步计算值的中间容器，
+ *    <code>AsyncCallback</code>，它可以在任务完成时执行，<code>AsyncExecutor</code> 管理异步任务的执行。
+ * <p>main 方法显示了异步调用的示例流程。 主线程启动多个具有可变持续时间的任务，然后继续自己的工作。 当主线程完成它的工作时，
+ * 它会收集异步任务的结果。 其中两个任务使用回调处理，这意味着当任务完成时会立即执行回调。
+ * <p>异步结果和回调之间线程使用的显着区别在于异步结果收集在主线程中，而回调在工作线程中执行。 使用线程池时应注意这一点。
+ * <p>Java 提供了自己的异步方法调用模式实现。 FutureTask、CompletableFuture 和 ExecutorService
+ * 是这种模式的真实世界实现。但由于并行编程的性质，实现不是那么简单。 此示例并未考虑所有可能的场景，而是提供了一个有助于
+ * 理解该模式的简单版本。
+ *
+ * <p>*略难理解，需要多看，复习*
+ *
  * @see AsyncResult
  * @see AsyncCallback
  * @see AsyncExecutor
@@ -59,71 +74,75 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class App {
 
-  /**
-   * Program entry point.
-   */
-  public static void main(String[] args) throws Exception {
-    // construct a new executor that will run async tasks
-    var executor = new ThreadAsyncExecutor();
+    /**
+     * Program entry point.
+     */
+    public static void main(String[] args) throws Exception {
+        // construct a new executor that will run async tasks
+        var executor = new ThreadAsyncExecutor();
 
-    // start few async tasks with varying processing times, two last with callback handlers
-    final var asyncResult1 = executor.startProcess(lazyval(10, 500));
-    final var asyncResult2 = executor.startProcess(lazyval("test", 300));
-    final var asyncResult3 = executor.startProcess(lazyval(50L, 700));
-    final var asyncResult4 = executor.startProcess(lazyval(20, 400),
-        callback("Deploying lunar rover"));
-    final var asyncResult5 =
-        executor.startProcess(lazyval("callback", 600), callback("Deploying lunar rover"));
+        // start few async tasks with varying processing times, two last with callback handlers
+        final var asyncResult1 = executor.startProcess(lazyval(10, 500));
+        final var asyncResult2 = executor.startProcess(lazyval("test", 300));
+        final var asyncResult3 = executor.startProcess(lazyval(50L, 700));
+        final var asyncResult4 =
+                executor.startProcess(lazyval(20, 400), callback("Deploying lunar rover"));
+        final var asyncResult5 =
+                executor.startProcess(lazyval("callback", 600), callback("Deploying lunar rover"));
 
-    // emulate processing in the current thread while async tasks are running in their own threads
-    Thread.sleep(350); // Oh boy, we are working hard here
-    log("Mission command is sipping coffee");
+        // emulate processing in the current thread while async tasks are running in their own threads
+        Thread.sleep(350); // Oh boy, we are working hard here
+        log("Mission command is sipping coffee");
 
-    // wait for completion of the tasks
-    final var result1 = executor.endProcess(asyncResult1);
-    final var result2 = executor.endProcess(asyncResult2);
-    final var result3 = executor.endProcess(asyncResult3);
-    asyncResult4.await();
-    asyncResult5.await();
+        // wait for completion of the tasks
+        final var result1 = executor.endProcess(asyncResult1);
+        final var result2 = executor.endProcess(asyncResult2);
+        final var result3 = executor.endProcess(asyncResult3);
+        //这两个await()有没有结果都一样？
+        asyncResult4.await();
+        asyncResult5.await();
 
-    // log the results of the tasks, callbacks log immediately when complete
-    log("Space rocket <" + result1 + "> launch complete");
-    log("Space rocket <" + result2 + "> launch complete");
-    log("Space rocket <" + result3 + "> launch complete");
-  }
+        // log the results of the tasks, callbacks log immediately when complete
+        log("Space rocket <" + result1 + "> launch complete");
+        log("Space rocket <" + result2 + "> launch complete");
+        log("Space rocket <" + result3 + "> launch complete");
+    }
 
-  /**
-   * Creates a callable that lazily evaluates to given value with artificial delay.
-   *
-   * @param value       value to evaluate
-   * @param delayMillis artificial delay in milliseconds
-   * @return new callable for lazy evaluation
-   */
-  private static <T> Callable<T> lazyval(T value, long delayMillis) {
-    return () -> {
-      Thread.sleep(delayMillis);
-      log("Space rocket <" + value + "> launched successfully");
-      return value;
-    };
-  }
+    /**
+     * Creates a callable that lazily evaluates to given value with artificial delay.
+     *
+     * @param value       value to evaluate
+     * @param delayMillis artificial delay in milliseconds
+     * @return new callable for lazy evaluation
+     */
+    private static <T> Callable<T> lazyval(T value, long delayMillis) {
+        return () -> {
+            Thread.sleep(delayMillis);
+            log("Space rocket <" + value + "> launched successfully");
+            return value;
+        };
+    }
 
-  /**
-   * Creates a simple callback that logs the complete status of the async result.
-   *
-   * @param name callback name
-   * @return new async callback
-   */
-  private static <T> AsyncCallback<T> callback(String name) {
-    return (value, ex) -> {
-      if (ex.isPresent()) {
-        log(name + " failed: " + ex.map(Exception::getMessage).orElse(""));
-      } else {
-        log(name + " <" + value + ">");
-      }
-    };
-  }
+    /**
+     * Creates a simple callback that logs the complete status of the async result.
+     *
+     * @param name callback name
+     * @return new async callback
+     */
+    private static <T> AsyncCallback<T> callback(String name) {
 
-  private static void log(String msg) {
-    LOGGER.info(msg);
-  }
+        return (value, ex) -> {
+            if (ex.isPresent()) {
+                log(name + " failed: " + ex.map(Exception::getMessage).orElse(""));
+            } else {
+                //Deploying lunar rover <20>
+                //Deploying lunar rover <callback>
+                log(name + " <" + value + ">");
+            }
+        };
+    }
+
+    private static void log(String msg) {
+        LOGGER.info(msg);
+    }
 }
