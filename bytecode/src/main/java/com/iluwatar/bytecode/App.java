ff30b49f573fa81e2bb7893b5f615b5bea223421
whalenchan <whalenchan330@gmail.com>
2021-06-23 09:31:13 +0800

    提交字节码模式注释

--- bytecode/src/main/java/com/iluwatar/bytecode/App.java
+++ bytecode/src/main/java/com/iluwatar/bytecode/App.java
@@ -38,6 +38,13 @@ import lombok.extern.slf4j.Slf4j;
  * long due to slow compile times or other tooling issues. It has too much trust. If you want to
  * ensure the behavior being defined can’t break the game, you need to sandbox it from the rest of
  * the codebase.
+ *
+ * <p>字节码模式的目的是通过将数据编码为虚拟机的指令来为行为提供数据的灵活性。 指令集定义了可以执行的低级操作。 一系列指令被编码为字节序列。
+ * 虚拟机一次一条地执行这些指令，将堆栈用于中间值。 通过组合指令，可以定义复杂的高级行为。
+ *
+ * <p>当需要定义大量行为并且实现引擎不是一个好的选择时，应该使用这种模式，因为它的级别太低，由于编译时间慢或其他工具问题，迭代时间太长。
+ * 它有太多的信任。 如果您想确保定义的行为不会破坏游戏，您需要将其与代码库的其余部分进行沙箱化。
+ *
  */
 @Slf4j
 public class App {
